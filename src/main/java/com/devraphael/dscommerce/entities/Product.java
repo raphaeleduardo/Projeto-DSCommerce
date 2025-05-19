c739e71fb6d170114c8cd4a71bbf2dfe6f8f639d
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-19 02:59:36 -0300

    many-to-many with association class

--- src/main/java/com/devraphael/dscommerce/entities/Product.java
+++ src/main/java/com/devraphael/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.devraphael.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -11,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,18 +23,19 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
 	private String imgUrl;
 
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Product() {
 	}
 
@@ -87,5 +90,14 @@ public class Product {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public List<Order> getOrders() {
+		// Para cada objeto X do tipo OrderItem, puxar só os do tipo Order
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
+
 }
