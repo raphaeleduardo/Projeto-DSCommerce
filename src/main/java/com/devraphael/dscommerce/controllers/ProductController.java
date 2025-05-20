7a9ceb304efa21256213293e035ed0f1921b03be
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-20 01:42:56 -0300

    add POST

--- src/main/java/com/devraphael/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devraphael/dscommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,15 +23,16 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public ProductDTO findById(@PathVariable Long id) {
 		return productService.findById(id);
-		
 	}
 	
 	@GetMapping
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		return productService.findAll(pageable);
-		
 	}
 	
-	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		return productService.insert(dto);
+	}
 	
 }
