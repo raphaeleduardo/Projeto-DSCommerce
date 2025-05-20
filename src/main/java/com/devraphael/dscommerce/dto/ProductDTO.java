cd281865664eb67b2d897ba2420f00c43b804ae2
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-05-20 18:11:03 -0300

    bean validation and custom responses

--- src/main/java/com/devraphael/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devraphael/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,23 @@ package com.devraphael.dscommerce.dto;
 
 import com.devraphael.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	
+	@Positive(message = "O preço precisa ser positivo")
 	private Double price;
 	private String imgUrl;
 
