3660fc002b08b2861b4dc0ae89272454e7ed3345
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-20 01:00:39 -0300

    ProductDTO with Categories

--- src/main/java/com/devraphael/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devraphael/dscommerce/dto/ProductDTO.java
@@ -1,26 +1,34 @@
 package com.devraphael.dscommerce.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devraphael.dscommerce.entities.Category;
 import com.devraphael.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
 	private Long id;
-	
+
 	@Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String name;
-	
+
 	@Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String description;
-	
+
 	@Positive(message = "O preço precisa ser positivo")
 	private Double price;
 	private String imgUrl;
+
+	@NotEmpty(message = "Deve ter pelo menos uma categoria")
+	private List<CategoryDTO> categories = new ArrayList<>();
 
 	public ProductDTO() {
 	}
@@ -40,46 +48,33 @@ public class ProductDTO {
 		description = entity.getDescription();
 		price = entity.getPrice();
 		imgUrl = entity.getImgUrl();
+		for (Category cat : entity.getCategories()) {
+			categories.add(new CategoryDTO(cat));
+		}
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public Double getPrice() {
 		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
 	}
 
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
+	public List<CategoryDTO> getCategories() {
+		return categories;
 	}
 
 }
--- src/main/java/com/devraphael/dscommerce/services/ProductService.java
+++ src/main/java/com/devraphael/dscommerce/services/ProductService.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devraphael.dscommerce.dto.CategoryDTO;
 import com.devraphael.dscommerce.dto.ProductDTO;
 import com.devraphael.dscommerce.dto.ProductMinDTO;
+import com.devraphael.dscommerce.entities.Category;
 import com.devraphael.dscommerce.entities.Product;
 import com.devraphael.dscommerce.repositories.ProductRepository;
 import com.devraphael.dscommerce.services.exceptions.DatabaseException;
@@ -21,18 +23,18 @@ import jakarta.persistence.EntityNotFoundException;
 public class ProductService {
 
 	@Autowired
-	private ProductRepository productRepository;
+	private ProductRepository repository;
 
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
-		Product product = productRepository.findById(id)
+		Product product = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		return new ProductDTO(product);
 	}
 
 	@Transactional(readOnly = true)
 	public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
-		Page<Product> result = productRepository.searchByName(name, pageable);
+		Page<Product> result = repository.searchByName(name, pageable);
 		return result.map(x -> new ProductMinDTO(x));
 	}
 
@@ -40,16 +42,16 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
 		copyDtoToEntity(dto, entity);
-		entity = productRepository.save(entity);
+		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
 
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
-			Product entity = productRepository.getReferenceById(id);
+			Product entity = repository.getReferenceById(id);
 			copyDtoToEntity(dto, entity);
-			entity = productRepository.save(entity);
+			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		} 
 		catch (EntityNotFoundException e) {
@@ -59,11 +61,11 @@ public class ProductService {
 
 	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id) {
-		if (!productRepository.existsById(id)) {
+		if (!repository.existsById(id)) {
 			throw new ResourceNotFoundException("Recurso não encontrado");
 		}
 		try {
-			productRepository.deleteById(id);
+			repository.deleteById(id);
 		} 
 		catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Falha de integridade referencial");
@@ -75,6 +77,13 @@ public class ProductService {
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category cat = new Category();
+			cat.setId(catDto.getId());
+			entity.getCategories().add(cat);
+		}
 	}
 
 }
