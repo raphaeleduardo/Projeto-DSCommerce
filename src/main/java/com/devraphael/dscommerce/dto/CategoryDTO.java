c158c0cad8e28cc9a163743f7ccc7d5107066723
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-20 01:27:29 -0300

    OrderDTO, get order by id

--- src/main/java/com/devraphael/dscommerce/dto/CategoryDTO.java
+++ src/main/java/com/devraphael/dscommerce/dto/CategoryDTO.java
@@ -7,8 +7,11 @@ public class CategoryDTO {
 	private Long id;
 	private String name;
 
-	public CategoryDTO() {
-	}
+	public CategoryDTO() {}
+		/* 
+		 * Criei o construtor vázio pois estava retornando a seguinte exception ao tentar inserir um produto novo:
+		 * org.springframework.http.converter.HttpMessageConversionException: Type definition error: [simple type, class com.devraphael.dscommerce.dto.CategoryDTO] 
+		*/
 
 	public CategoryDTO(Long id, String name) {
 		this.id = id;
---
+++ src/main/java/com/devraphael/dscommerce/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.devraphael.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devraphael.dscommerce.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
