f92b94de1945a2df669af160cd4c9b9ec8c19f65
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-20 12:59:48 -0300

    GET Categories

---
+++ src/main/java/com/devraphael/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devraphael.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devraphael.dscommerce.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
